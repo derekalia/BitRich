2fb0e01d8536b7910722c0252dbf51b5d99a7788
Aaron Rassiq <amrassiq@dons.usfca.edu>
2024-11-01 17:25:22 -0700

    agent and requirement changes

---
+++ doubleCheckAgent.py
@@ -0,0 +1,25 @@
+
+'''
+* Screenshot retreival (call from the json file)
+* take pass from the double check call (what should have been created)
+* consolidate into one input
+* pass to llama
+
+return True or False to the calling function
+'''
+
+def retrieveJson() -> dict:
+    #request screenshot and convert to json through the api cv model
+
+    currentPageDict = {}
+
+    return currentPageDict
+
+
+
+def doubleCheckAgent(desiredState: str):
+    
+    currentPageDict = retrieveJson()
+    
+    
+    return 
